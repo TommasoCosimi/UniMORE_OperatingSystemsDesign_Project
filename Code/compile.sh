8e73923befc4207c54c6b35f1cc21bab3dff8d12
Tommaso Cosimi <tommaso.cosimi@outlook.it>
2023-12-27 12:21:51 +0100

    Revised Compilation Script

--- Code/compile.sh
+++ Code/compile.sh
@@ -13,3 +13,10 @@ pwd
 
 echo "Compilo le classi in bin"
 javac -d ../../bin/SupportFiles ./*.java
+
+echo "Creo dei file di supporto per l'utilizzo di alcune classi"
+cd ../../bin/SupportFiles
+echo "File di esempio contenente una riga." > FileConUnaRiga.txt
+echo "File di esempio contenente più righe." > FileConPiuRighe.txt
+echo "Riga aggiuntiva." >> FileConPiuRighe.txt
+echo "Ulteriore riga aggiuntiva." >> FileConPiuRighe.txt
